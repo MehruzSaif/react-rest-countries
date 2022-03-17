513e6c3f9cea76506e5cbf3b501b3e61ae5b0554
MehruzSaif <mehruzsaif007@gmail.com>
2022-03-18 02:40:42 +0600

    added flag property

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './Country.css';
 
 const Country = (props) => {
-    const { name, area, population, region } = props.country;
+    const { name, flags, area, population, region } = props.country;
 
     return (
         <div className='country'>
 
             <h3>Name: {name.common}</h3>
+            <img src={flags.png} alt="" />
             <p>Area: {area}</p>
             <p><small>Population: {population}</small></p>
             <p><small><b>Region: {region}</b></small></p>
