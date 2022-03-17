49718cb21dbda9239c5b507bcb288dd2a1466d45
MehruzSaif <mehruzsaif007@gmail.com>
2022-03-17 18:48:04 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Welcome to my react website</h2>
+        </div>
+    );
+};
+
+export default Header;
