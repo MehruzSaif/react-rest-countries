8d0e6eea477658e28853ecd05ff1af90be8df8d9
MehruzSaif <mehruzsaif007@gmail.com>
2022-03-18 02:30:21 +0600

    column grid layout modified

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -18,12 +19,14 @@ const Countries = () => {
             {/* {
                 countries.map(country => console.log(country))
             } */}
-            {
-                countries.map(country => <Country
-                    country={country}
-                >
-                </Country>)
-            }
+            <div className='countries-container'>
+                {
+                    countries.map(country => <Country
+                        country={country}
+                    >
+                    </Country>)
+                }
+            </div>
         </div>
     );
 };
